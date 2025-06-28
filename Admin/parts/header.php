4745f50bdb60c0b0dc4c65843953bd92f401ca70
kuratanimasato <tyutyumasato@gmail.com>
2025-06-29 03:35:19 +0900

    Add Font Awesome

--- Admin/parts/header.php
+++ Admin/parts/header.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>管理画面ログイン</title>
     <link rel="icon" href="favicon.ico" />
+    <!-- Font Awesome CDN（例：v6） -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
       integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
